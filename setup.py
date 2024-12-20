0c58231db8f658f2b83a9ac32cd1f37d159477e7
Rick Lamers <ricklamers@gmail.com>
2024-12-20 13:25:21 +0100

    release: bump version to 0.4.2 in setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open('README.md', 'r', encoding='utf-8') as f:
 
 setup(
     name='shell-ai',
-    version='0.4.1',
+    version='0.4.2',
     author='Rick Lamers',
     long_description=long_description,
     long_description_content_type='text/markdown',
